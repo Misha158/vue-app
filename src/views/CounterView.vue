8b7df10b463904824eb9e70bcfe1b06e7dcb88b4
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-09-27 20:30:34 +0300

    Added counter page

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+import { onMounted, ref } from 'vue'
+
+const counter = ref(0)
+
+const increment = () => {
+  counter.value++
+}
+
+const decrement = () => {
+  counter.value--
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${counter.value}.`)
+})
 </script>
 
 <template>
   <div class="greetings">
     <div>Counter page</div>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+    <div>Counter: {{ counter }}</div>
   </div>
 </template>
 
