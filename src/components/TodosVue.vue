1217a56c447fcb268df9e154627599c353bdc140
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-09-28 14:06:18 +0300

    Added delete, update handlers

--- src/components/TodosVue.vue
+++ src/components/TodosVue.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { PropType } from 'vue'
+import { ref, type PropType } from 'vue'
 
 defineProps({
   todos: {
@@ -7,6 +7,23 @@ defineProps({
     required: true
   }
 })
+
+const emit = defineEmits(['deleteTodo', 'updateTodo'])
+const isShowUpdateInput = ref(0)
+const newTitle = ref('')
+
+const onDeleteTodo = (id: number) => {
+  emit('deleteTodo', id)
+}
+
+const onUpdateTodo = (id: number) => {
+  isShowUpdateInput.value = id
+}
+
+const onSaveUpdatedTodo = (id: number, title: string) => {
+  emit('updateTodo', id)
+  isShowUpdateInput.value = 0
+}
 </script>
 
 <template>
@@ -14,10 +31,52 @@ defineProps({
     <div class="greetings">
       <div>Todos page</div>
       <div v-for="todo in todos" :key="todo.id">
-        <div>{{ todo.title }}</div>
+        <div class="todo__container">
+          <div class="todo">
+            <div>{{ todo.id }}.</div>
+            <div class="title" v-show="!isShowUpdateInput || isShowUpdateInput !== todo.id">
+              {{ todo.title }}
+            </div>
+            <div class="title" v-show="isShowUpdateInput === todo.id">
+              <input
+                v-model="todo.title"
+                @input="newTitle = ($event.target as HTMLInputElement)?.value"
+              />
+              <button @click="onSaveUpdatedTodo(todo.id, newTitle)">Save</button>
+            </div>
+          </div>
+          <div class="actions">
+            <button @click="onDeleteTodo(todo.id)">Delete</button>
+            <button @click="onUpdateTodo(todo.id)">Update</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.todo__container {
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+  background-color: aquamarine;
+  border-radius: 5px;
+  color: black;
+  margin: 10px 0;
+  padding: 10px;
+}
+
+.todo {
+  display: flex;
+}
+
+.title {
+  margin-left: 5px;
+}
+
+.actions {
+  background-color: bisque;
+  border-radius: 5px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,13 +13,27 @@ const todos = ref([
 const addNewTodo = (todo: any) => {
   todos.value.push(todo)
 }
+
+const onDeleteTodo = (id: number) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id)
+}
+
+const onSaveUpdatedTodo = (id: number, title: string) => {
+  todos.value = todos.value.map((todo) => {
+    if (todo.id === id) {
+      return { id: todo.id, title }
+    }
+
+    return todo
+  })
+}
 </script>
 
 <template>
   <div class="greetings">
     <CounterVue />
     <FormVue @createNewTodo="addNewTodo" />
-    <TodosVue :todos="todos" />
+    <TodosVue @deleteTodo="onDeleteTodo" @onSaveUpdatedTodo="onSaveUpdatedTodo" :todos="todos" />
   </div>
 </template>
 
